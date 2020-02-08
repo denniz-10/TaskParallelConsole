f09606cb1086eca73bbe1f04628ab0d36432f155
DenilsonRamiro <denilson.ramiro@it4solution.com.br>
2020-02-08 12:55:27 -0300

    First Commit

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TaskParallel
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var relogio = new Stopwatch();
+            relogio.Start();
+            CozinharMacarrao();
+            RefogarMolho();
+            relogio.Stop();
+            Console.WriteLine($"tempo: {relogio.ElapsedMilliseconds / 1000.0} \n");
+
+            Console.WriteLine("Tarefa paralela\n");
+            relogio.Reset();
+            relogio.Restart();
+            Parallel.Invoke(() => CozinharMacarrao(),
+            () => RefogarMolho());
+            relogio.Stop();
+            Console.WriteLine($"tempo: {relogio.ElapsedMilliseconds / 1000.0}");
+
+        }
+
+        static void CozinharMacarrao()
+        {
+            Console.WriteLine("Cozinhando macarrao");
+            Thread.Sleep(1000);
+            Console.WriteLine("macarrao ja esta cozido");
+            Console.WriteLine();
+        }
+
+        static void RefogarMolho()
+        {
+            Console.WriteLine("Refogando molho");
+            Thread.Sleep(2000);
+            Console.WriteLine("Molho ja esta refogado");
+            Console.WriteLine();
+        }
+    }
+}
